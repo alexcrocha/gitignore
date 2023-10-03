f3fd8737b5af781b18fbd7cfe923707883113105
Alex Rocha <rocha.amc@gmail.com>
2023-10-02 20:17:21 -0700

    Make it DRYer and easier to test

--- index.ts
+++ index.ts
@@ -1,32 +1,40 @@
 #!/usr/bin/env ts-node
 
 import axios from 'axios';
-import fs from 'fs';
-import { promisify } from 'util';
-
+import fs from 'fs/promises';
 import { program } from 'commander';
 
 program
   .option('-l, --languages <type...>', 'comma separated languages or frameworks')
-  .action(async () => {
-    try {
-      const isLanguagesFlagProvided = process.argv.includes('-l') || process.argv.includes('--languages');
-      const options = program.opts();
-      const languages = isLanguagesFlagProvided ? options.languages.join(',') : program.args.join(',');
-
-      const gitignoreContent = await fetchGitignore(languages);
-
-      const writeFileAsync = promisify(fs.writeFile);
-      await writeFileAsync('.gitignore', gitignoreContent);
-
-      console.log('Generated .gitignore file');
-    } catch (error) {
-      console.error(error);
-    }
-  })
+  .action(main)
   .parse(process.argv);
+
+async function main(): Promise<void> {
+  try {
+    const languages = getLanguagesFromOptionsOrArgs();
+    const gitignoreContent = await fetchGitignore(languages);
+    await writeGitignoreFile(gitignoreContent);
+    console.log('Generated .gitignore file');
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+function getLanguagesFromOptionsOrArgs(): string {
+  const isLanguagesFlagProvided = process.argv.includes('-l') || process.argv.includes('--languages');
+  const options = program.opts();
+  return isLanguagesFlagProvided ? options.languages.join(',') : program.args.join(',');
+}
 
 async function fetchGitignore(languages: string): Promise<string> {
   const response = await axios.get(`https://www.toptal.com/developers/gitignore/api/${languages}`);
   return response.data;
+}
+
+async function writeGitignoreFile(content: string): Promise<void> {
+  await fs.writeFile('.gitignore', content);
+}
+
+function handleError(error: any): void {
+  console.error(`An error occurred: ${error.message}`);
 }
